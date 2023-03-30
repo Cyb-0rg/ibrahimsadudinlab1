09e735e9e8e42ade0bf0e861b5ef9e5d89c43529
Ibrahim Sadudin Kedir <ibrahimsdudin@gmail.com>
2023-03-30 12:12:09 +0300

    feat: week5 js
    includes form sunmission

--- js/week5.js
+++ js/week5.js
@@ -6,8 +6,52 @@ var num3 = 0;
 
 //alert(`total: ${num1+num2}`)
 
-
-while (num3 < 10){
-    console.log(num3);
-    num3++;
+function submitForm(){
+    let element = document.getElementById("name");
+    let name = element.value;
+    alert(`dear ${name} your form has been sent. Thank you `);
+    //alert(`dear XXXX your form has been sent. Thank you `);
 }
+
+function countUp(){
+    
+    let element = document.getElementById("btn-count-number");
+    num3++;
+    element.innerHTML = num3;
+}
+
+function onhover(){
+    let element = document.getElementById("cnt-ttl");
+    let element2 = document.getElementById("btn-count-number");
+    let element3 = document.getElementById("contact-info-text");
+    let element4 = document.getElementById("clickMe-msg");
+
+    element.style.color = "green";
+    element2.style.color = "green";
+    element3.style.color = "green";
+    element4.style.color = "green";
+
+
+}
+
+function onMouseLeave(){
+    let element = document.getElementById("cnt-ttl");
+    let element2 = document.getElementById("btn-count-number");
+    let element3 = document.getElementById("contact-info-text");
+    let element4 = document.getElementById("clickMe-msg");
+
+
+    //element.style.color = "white";
+    element2.style.color = "white";
+    //element3.style.color = "white";
+    element4.style.color = "white";
+
+
+}
+
+
+while (num2 < 10){
+    console.log(num2);
+    num2++;
+}
+
